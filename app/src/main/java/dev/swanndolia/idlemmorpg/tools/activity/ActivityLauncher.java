99ce7bdaeda4e4b89236a90699785f4318f1ccb7
swanndolia <Swanndolia@gmail.com>
2023-06-28 20:08:49 +0200

    better naming

--- app/src/main/java/dev/swanndolia/idlemmorpg/tools/activity/ActivityLauncher.java
+++ app/src/main/java/dev/swanndolia/idlemmorpg/tools/activity/ActivityLauncher.java
@@ -7,24 +7,24 @@ import android.content.Intent;
 import dev.swanndolia.idlemmorpg.characters.Player;
 
 public class ActivityLauncher {
-    public ActivityLauncher(Context fromActivity, Class<?> toActivity) {
-        Intent intent = new Intent(fromActivity, toActivity);
-        fromActivity.startActivity(intent);
-        ((Activity) fromActivity).finish();
+    public ActivityLauncher(Context previousActivity, Class<?> nextActivity) {
+        Intent intent = new Intent(previousActivity, nextActivity);
+        previousActivity.startActivity(intent);
+        ((Activity) previousActivity).finish();
     }
 
-    public ActivityLauncher(Context fromActivity, Class<?> toActivity, Player player) {
-        Intent intent = new Intent(fromActivity, toActivity);
+    public ActivityLauncher(Context fromActivity, Class<?> nextActivity, Player player) {
+        Intent intent = new Intent(fromActivity, nextActivity);
         intent.putExtra("player", player);
         fromActivity.startActivity(intent);
         ((Activity) fromActivity).finish();
     }
 
-    public ActivityLauncher(Context fromActivity, Class<?> toActivity, Player player, String extraName, String extraMeta) {
-        Intent intent = new Intent(fromActivity, toActivity);
+    public ActivityLauncher(Context previousActivity, Class<?> nextActivity, Player player, String extraName, String extraMeta) {
+        Intent intent = new Intent(previousActivity, nextActivity);
         intent.putExtra("player", player);
         intent.putExtra(extraName, extraMeta);
-        fromActivity.startActivity(intent);
-        ((Activity) fromActivity).finish();
+        previousActivity.startActivity(intent);
+        ((Activity) previousActivity).finish();
     }
 }
